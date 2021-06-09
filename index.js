9527a266619028698229114808d04a1c22ad19ed
Charlie Macnamara <54804572+WardenScotland@users.noreply.github.com>
2021-06-09 15:32:16 +0100

    change standards

--- index.js
+++ index.js
@@ -1,56 +1,49 @@
-//parse text from <p> tags
-const result = document.querySelectorAll("[data-screenreaderLanguage]");
-const textArray = [];
-result.forEach(e => textArray.push(e.innerText));
+const result = document.querySelectorAll('[data-screenreaderLanguage]')
+const textArray = []
+result.forEach(e => textArray.push(e.innerText))
 
-const language = document.querySelector("[data-screenreaderLanguage]").dataset.screenreaderlanguage;
+const language = document.querySelector('[data-screenreaderLanguage]').dataset.screenreaderlanguage
 
-//create file for Uppy
-let mytextfile = new File([textArray], "mytextfile.txt", {type: "text/plain"});
+let mytextfile = new File([textArray], 'mytextfile.txt', {type: 'text/plain'})
 
+const buttonEl = document.getElementById('button_event')
+const tmpGif = document.createElement('img')
+const audioPlayer = document.createElement('AUDIO')
 
-//Create ELements
-const buttonEl = document.getElementById("button_event");
-const tmpGif = document.createElement("img");
-const audioPlayer = document.createElement("AUDIO");
+tmpGif.src = 'loader.gif'
+tmpGif.width = 100
+tmpGif.height = 100
+tmpGif.id = 'tmpGif'
 
-tmpGif.src = "loader.gif";
-tmpGif.width = 100;
-tmpGif.height = 100;
-tmpGif.id = "tmpGif";
+const runScript = async () => { 
+  buttonEl.parentNode.replaceChild(tmpGif, buttonEl)
+  
+  const Uppy = Robodog.upload([mytextfile], {
+    waitForEncoding: true,
+    params: {
+      auth: { key: '6f9ece25ada84dde9bbcfdcb652b5b6a' },
+      steps: {
+      ':original': {
+        robot: '/upload/handle',
+      },
+      speach: {
+        use: ':original',
+        robot: '/text/speak',
+        provider: 'aws',
+        target_language: language
+        }
+        }
+      }
+    }).then((bundle) => {
+      let audio_url = bundle.results[0].url
 
-//Event listen
-const runScript = async () => {
-    try {
-        buttonEl.parentNode.replaceChild(tmpGif, buttonEl);
+      audioPlayer.setAttribute('src', audio_url)
+      audioPlayer.setAttribute('controls', 'controls')
+      audioPlayer.setAttribute('autoplay', 'autoplay')
 
-        const resultPromise = await Robodog.upload([mytextfile], {
-            waitForEncoding: true,
-            params: {
-                auth: { key: "YOUR_AUTH_KEY" },
-                steps: {
-                ":original": {
-                    robot: "/upload/handle",
-                },
-                speach: {
-                    use: ":original",
-                    robot: "/text/speak",
-                    provider: "aws",
-                    target_language: language
-                }
-                }
-            }
-            });
-
-        let audio_url = resultPromise.results[0].url;
-
-        audioPlayer.setAttribute("src", audio_url);
-        audioPlayer.setAttribute("controls", "controls");
-        audioPlayer.setAttribute("autoplay", "autoplay");
-
-        tmpGif.parentNode.replaceChild(audioPlayer, tmpGif);
-    } catch (err) {
-        console.error(err);
-        tmpGif.parentNode.replaceChild(buttonEl, tmpGif);
+      tmpGif.parentNode.replaceChild(audioPlayer, tmpGif)
+    }).catch((error) => {
+      console.error(error)
+      tmpGif.parentNode.replaceChild(buttonEl, tmpGif)
+      })
     }
-};
